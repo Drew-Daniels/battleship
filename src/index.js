6ef0bcd4f39734248284740e8b8aaf052cbc225b
Drew Daniels <drew@thedaniels.com>
2022-02-10 14:35:20 -0600

    Added an area where a user will be able to drag their ships to the board

--- src/index.js
+++ src/index.js
@@ -1,28 +1,30 @@
 import { Ship, Gameboard, Player } from './app';
 
 function main() {
-  
+
+  // create Player objects
   const p1 = Player();
   const p2 = Player(false);
 
+  // create Gamemboard objects
   const p1GB = Gameboard();
   const p2GB = Gameboard(false);
 
-  // p1 ships
+  // create Player 1 Ship objects
   const p1Carrier = Ship('carrier');
   const p1Battleship = Ship('battleship');
   const p1Cruiser = Ship('cruiser');
   const p1Submarine = Ship('submarine');
   const p1Destroyer = Ship('destroyer');
-  // p2 ships
+  
+  // create Player 2 Ship objects
   const p2Carrier = Ship('carrier');
   const p2Battleship = Ship('battleship');
   const p2Cruiser = Ship('cruiser');
   const p2Submarine = Ship('submarine');
   const p2Destroyer = Ship('destroyer');
 
-  // add event listeners, draw DOM elements etc.
-  console.log(p1.isHuman());
+
 }
 
 main();
